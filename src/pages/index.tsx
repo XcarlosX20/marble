6ab748cc13a32696c8cb18c959e8d141fabee60c
Carlos Sierra <carlossierra850@gmail.com>
2023-04-28 13:33:35 -0400

    deploy of current work for review

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
-import {Circle} from '@/Components/Carousel/Circle'
 import Header from '@/Components/Layout/Header'
 import { Hero } from '@/Components/sections/Hero'
-import {SelectMarble} from '@/Components/sections/SelectMarble'
 import { font } from '@/Components/Layout/theme'
 import { Whyus } from '@/Components/sections/Whyus'
 export default function Home() {
