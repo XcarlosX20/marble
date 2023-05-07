e2f827a7ac937cbaca3f079b3c0ad0825db18589
Carlos Sierra <carlossierra850@gmail.com>
2023-05-07 19:06:22 -0400

    same text

--- src/Components/sections/Ourstory.tsx
+++ src/Components/sections/Ourstory.tsx
@@ -6,15 +6,22 @@ const OurStory = () => {
   return (
     <CScrollIntoView>
       <section className="bg-gray py-[70px]">
+        <h2 className="md:hidden text-fontWhite uppercase text-center text-6xl font-bold mb-8">
+          Our Story
+        </h2>
         <div className="flex flex-col mx-8 md:ml-0 md:mr-20 md:flex-row gap-4">
           <div className="relative mx-auto md:my-auto lg:m-0  ">
             <div className="relative z-20">
-              <Image src={imageOurStory} alt={"image of Our story"} />
+              <Image
+                className="rounded"
+                src={imageOurStory}
+                alt={"image of Our story"}
+              />
             </div>
-            <div className="absolute top-[-15px] right-[-15px] z-10 w-32 h-32 t bg-dark-100 z-index-10"></div>
+            <div className="absolute top-[-15px] right-[-15px] z-10 w-32 h-32 t rounded bg-dark-400 z-index-10"></div>
           </div>
           <div className="flex flex-col md:w-1/2 md:pl-8 md:pr-4 ">
-            <h2 className="text-fontWhite uppercase text-3xl font-bold mb-3">
+            <h2 className="hidden md:block text-fontWhite uppercase md:text-4xl lg:text-6xl font-bold mb-3">
               Our Story
             </h2>
             <h4 className="font-semibold text-lg">
@@ -22,12 +29,13 @@ const OurStory = () => {
             </h4>
             <div className="bg-blue-100 h-[2px] w-4/12 mb-4"></div>
             <p className="text-fontWhite">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores
-              doloribus corrupti vero et odit quam dolor beatae magnam. Fugiat,
-              eius. Facilis nam dolores in velit, atque libero id, facere
-              tempore beatae similique labore unde suscipit inventore voluptas
-              autem odio itaque, nesciunt repellat minima maxime optio dolor.
-              Nemo deserunt laborum eaque.
+              We are experts in marble work, committed to quality and attention
+              to detail. With a focus on kitchen, bathroom, flooring, and custom
+              designs, we offer a range of options to enhance your living
+              spaces. Trust us to provide expert guidance and craftsmanship for
+              stunning and functional results that elevate your space. Add a
+              touch of luxury to your home or business with our timeless marble
+              work.
             </p>
           </div>
         </div>
