f332c652c532f803d878cb235eaa3e3220f043a8
eduardidn <eduardo.lopez@clevertech.biz>
2023-05-14 23:35:47 -0500

    fix: hero styles

--- src/Components/sections/Hero.tsx
+++ src/Components/sections/Hero.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import bg from "public/assets/img/bg-hero.jpg";
 import { Button } from "../Layout/Button";
+import arrow from "public/assets/svg/arrow.svg";
 import kitchen from "public/assets/svg/Vectorkitchen.svg";
 import customDesign from "public/assets/svg/ruler_svgrepo.comcustomdesign.svg";
 import bathroom from "public/assets/svg/Vectorbathroom.svg";
@@ -43,32 +44,33 @@ const Hero = () => {
         "Create a one-of-a-kind feature for your space with our custom marble design services.",
     },
   ];
+
   return (
     <>
-      <div className="bg-light-200 relative">
-        <div className="absolute overflow-hidden w-full h-full z-5 top-0 left-0 right-0 bottom-0">
+      <div className="relative bg-gray">
+        <div className="absolute overflow-hidden w-full h-full z-6 top-0 left-0 right-0 bottom-0">
           <Image
-            className="h-[100vh] w-[100vw] object-cover"
+            className="h-[90vh] w-[100vw] object-cover"
             src={bg}
             alt="bg-hero"
           />
         </div>
         <div className="relative w-full h-full z-10 pb-14">
-          <div className="pt-[225px] px-8 md:px-20 lg:px-[190px]">
-            <div className="flex flex-col gap-14 xl:gap-28">
-              <article className="flex m-h-[20rem] flex-col items-start gap-4 w-full md:max-w-[646px]">
-                <h2 className="font-bold text-3xl">
+          <div className="pt-[225px] px-8 md:px-20 max-w-[1148px] m-auto">
+            <div className="flex flex-col">
+              <article className="m-h-[20rem]w-full md:max-w-[646px]">
+                <h2 className="font-bold text-4xl">
                   Crafting Timeless beauty with the finest quality marble
                   materials
                 </h2>
-                <p className="font-medium md:max-w-[450px]">
+                <p className="font-medium md:max-w-[450px] mt-[25px]">
                   Experience the beauty and functionality of marble. Our expert
                   work enhances spaces. Trust us for quality and attention to
                   detail.
                 </p>
-                <Button>FIND OUT MORE</Button>
+                <Button className="mt-[25px]">FIND OUT MORE</Button>
               </article>
-              <div className="md:hidden ">
+              <div className="md:hidden">
                 <Slider className="w-60 mx-auto" {...sliderProps}>
                   {heroCards.map((card) => (
                     <div key={card.name} className="bg-[#2D2D2D] h-[20rem] p-8">
@@ -83,15 +85,26 @@ const Hero = () => {
                   ))}
                 </Slider>
               </div>
-              <div className="md:grid grid-cols-4 gap-4 hidden">
+              <div className="md:grid grid-cols-4 gap-4 hidden mt-[215px]">
                 {heroCards.map((card) => (
-                  <div key={card.name} className="bg-[#2D2D2D] p-8">
-                    <div className="flex flex-col gap-4">
-                      <Image src={card.icon} alt="kitchen" />
-                      <p className="text-blue-100 font-medium uppercase">
-                        {card.name}
-                      </p>
-                      <p className="text-fontWhite">{card.description}</p>
+                  <div
+                    key={card.name}
+                    className="bg-[#2D2D2D] p-[35px] h-[370px]"
+                  >
+                    <div className="flex flex-col justify-between h-full">
+                      <div>
+                        <Image src={card.icon} alt="kitchen" />
+                        <p className="text-blue-100 font-medium uppercase mt-[28px]">
+                          {card.name}
+                        </p>
+                        <p className="text-fontWhite mt-[23px] text-sm">
+                          {card.description}
+                        </p>
+                      </div>
+                      <div className="text-fontWhite flex space-x-2 hover:underline decoration-1 hover:cursor-pointer">
+                        <p>View our work</p>
+                        <Image src={arrow} alt="arrow" />
+                      </div>
                     </div>
                   </div>
                 ))}
